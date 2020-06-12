655fb8815d46ec9463e867325a2f1e37bb9b7fd5
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-11 23:00:53 -0400

    Created web server config

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const index = require('./routes/index');
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
