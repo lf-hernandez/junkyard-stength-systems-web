f4694f116bc7678b6cf071ae8e02efb7c9cf8880
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-13 21:49:29 -0400

    Removed mock table

--- client/src/views/Clients.vue
+++ client/src/views/Clients.vue
@@ -1,36 +1,7 @@
 <template>
   <div class="Clients">
     <div class="container">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Week</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>54</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>7</td>
-          </tr>
-        </tbody>
-      </table>
+    
     </div>
   </div>
 </template>
