5ddb98489239f673972674f2e0c97dfabb216cb6
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-17 12:44:17 -0400

    Replaced bootstrap via cdn with npm package

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/src/jquery.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
