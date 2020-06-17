c7eef2902681abd3d1e5756ffbc36d76cbae3986
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-17 13:01:54 -0400

    icons

--- src/components/clients/client-card/ClientCard.vue
+++ src/components/clients/client-card/ClientCard.vue
@@ -17,12 +17,19 @@
                 </div>
                 <div class="p-4">
                     <button class="btn btn-lg btn-primary mr-3" @click="pushViewRoute">
+                        <svg class="bi bi-person-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                            <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        </svg>
                         View
                     </button>
 
                     <button class="btn btn-lg btn-success" @click="pushEditRoute">
+                        <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
+                            <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
+                        </svg>
                         Edit
-                        <i class="far fa-pencil"></i>
                     </button>
                 </div>
             </div>
--- src/components/clients/client-details/client-details-form/ClientDetailsForm.vue
+++ src/components/clients/client-details/client-details-form/ClientDetailsForm.vue
@@ -5,12 +5,14 @@
         <form>
             <div class="form-group">
                 <label for="first-name">First Name</label>
-                <input type="text" class="form-control" id="first-name" aria-describedby="first-name" v-model="client.FirstName" />
+                <input type="text" class="form-control" id="first-name" aria-describedby="first-name"
+                       v-model="client.FirstName"/>
             </div>
 
             <div class="form-group">
                 <label for="last-name">Last Name</label>
-                <input type="text" class="form-control" id="last-name" aria-describedby="last-name" v-model="client.LastName"/>
+                <input type="text" class="form-control" id="last-name" aria-describedby="last-name"
+                       v-model="client.LastName"/>
             </div>
             <hr>
             <div class="form-group">
@@ -27,9 +29,17 @@
 
             <div class="form-group">
                 <label for="last-chek-in">Last Check-in</label>
-                <input type="tel" class="form-control" id="last-chek-in" aria-describedby="last-check-in" v-model="client.LastCheckIn"/>
+                <input type="tel" class="form-control" id="last-chek-in" aria-describedby="last-check-in"
+                       v-model="client.LastCheckIn"/>
             </div>
-            <button type="submit" class="mt-5 btn btn-block btn-primary">Save</button>
+            <div class="row mt-5">
+                <div class="col">
+                    <button type="button" class="btn btn-block btn-danger">Remove</button>
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-block btn-primary">Save</button>
+                </div>
+            </div>
         </form>
     </div>
 </template>
