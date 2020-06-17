7bd78812fed573981695e666d723841fb4fa3d1b
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-16 23:42:25 -0400

    Formatting

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 module.exports = {
     chainWebpack: config => {
-        const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-        types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+        types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)));
     }
-}
+};
 
-function addStyleResource (rule) {
+function addStyleResource(rule) {
     rule.use('style-resource')
         .loader('style-resources-loader')
         .options({
             patterns: [
-                path.resolve(__dirname, './src/styles/style.scss'),
+                path.resolve(__dirname, './src/styles/style.scss')
             ]
         });
 }
