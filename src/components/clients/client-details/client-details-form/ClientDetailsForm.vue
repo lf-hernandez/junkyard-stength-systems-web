a8b45b24654789f8f9b35dc273584f2f055339c0
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-17 14:30:41 -0400

    Split form fields into cols

--- src/components/clients/client-details/client-details-form/ClientDetailsForm.vue
+++ src/components/clients/client-details/client-details-form/ClientDetailsForm.vue
@@ -3,34 +3,42 @@
 <template>
     <div class="clientDetailsForm">
         <form>
-            <div class="form-group">
-                <label for="first-name">First Name</label>
-                <input type="text" class="form-control" id="first-name" aria-describedby="first-name"
-                       v-model="client.FirstName"/>
-            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="first-name">First Name</label>
+                        <input type="text" class="form-control" id="first-name" aria-describedby="first-name"
+                               v-model="client.FirstName"/>
+                    </div>
 
-            <div class="form-group">
-                <label for="last-name">Last Name</label>
-                <input type="text" class="form-control" id="last-name" aria-describedby="last-name"
-                       v-model="client.LastName"/>
-            </div>
-            <hr>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" v-model="client.Email"/>
-            </div>
+                    <div class="form-group">
+                        <label for="last-name">Last Name</label>
+                        <input type="text" class="form-control" id="last-name" aria-describedby="last-name"
+                               v-model="client.LastName"/>
+                    </div>
 
-            <div class="form-group">
-                <label for="phone">Phone</label>
-                <input type="text" class="form-control" id="phone" aria-describedby="phone" v-model="client.Phone"/>
-            </div>
+                    <hr>
 
-            <hr>
+                    <div class="form-group">
+                        <label for="last-chek-in">Last Check-in</label>
+                        <input type="tel" class="form-control" id="last-chek-in" aria-describedby="last-check-in"
+                               v-model="client.LastCheckIn"/>
+                    </div>
+                </div>
 
-            <div class="form-group">
-                <label for="last-chek-in">Last Check-in</label>
-                <input type="tel" class="form-control" id="last-chek-in" aria-describedby="last-check-in"
-                       v-model="client.LastCheckIn"/>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" v-model="client.Email"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="phone">Phone</label>
+                        <input type="text" class="form-control" id="phone" aria-describedby="phone"
+                               v-model="client.Phone"/>
+                    </div>
+                </div>
+
             </div>
             <div class="row mt-5">
                 <div class="col">
