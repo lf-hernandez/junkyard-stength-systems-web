5022cef05505fec848ac1fd77f22bcde7c379898
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-19 22:31:15 -0400

    Renoved old styles

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,10 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+  @import "./styles/style.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: $font-family-roboto;
 }
 </style>
 
