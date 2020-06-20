3bece912880e8867e82950594ef77f665618832d
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-20 03:09:44 -0400

    Update ExerciseLibrary.vue

--- src/views/exercise-library/ExerciseLibrary.vue
+++ src/views/exercise-library/ExerciseLibrary.vue
@@ -3,29 +3,33 @@
 <template>
     <div class="exerciseLibrary">
         <div class="container">
-            <div class="row mb-4">
-                <div class="col-md-4">
+            <div class="row mb-5">
+                <div class="col-4">
                     <h1 class="text-left font-weight-light m-0">Exercise Library</h1>
                 </div>
-                <div class="col-md-4 offset-4">
-                    <form class="form-inline">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                <div class="col-8">
+                    <form>
+                        <input class="form-control form-control-lg" type="text" placeholder="Search"
                                aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </div>
             </div>
+
             <div class="d-flex flex-nowrap">
-                <div class="flex-column flex-shrink visible-xl">
+                <div class="flex-column flex-shrink mr-4">
                     <ExerciseLibrarySideNav></ExerciseLibrarySideNav>
+
+                    <button class="btn btn-block btn-lg btn-dark mt-4">
+                        <font-awesome-icon icon="plus"></font-awesome-icon>
+                        Add Exercise
+                    </button>
                 </div>
 
                 <div class="flex-column flex-grow">
-                    <div class="container">
-                        <div class="row row-cols-1 row-cols-md-3">
-                            <div class="col mb-5" v-for="exercise in exercises" :key="exercise.id">
-                                <ExerciseLibraryCard v-on:click.native="onClick(exercise)" :exercise="exercise"></ExerciseLibraryCard>
-                            </div>
+                    <div class="row row-cols-1 row-cols-md-3">
+                        <div class="col mb-5" v-for="exercise in exercises" :key="exercise.id">
+                            <ExerciseLibraryCard v-on:click.native="onClick(exercise)"
+                                                 :exercise="exercise"></ExerciseLibraryCard>
                         </div>
                     </div>
                 </div>
