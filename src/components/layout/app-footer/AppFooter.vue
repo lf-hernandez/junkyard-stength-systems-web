99cbe52e066e02cd20c9173e1cce88844509659c
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-23 00:09:53 -0400

    Added social media and email footer anchors

--- src/components/layout/app-footer/AppFooter.vue
+++ src/components/layout/app-footer/AppFooter.vue
@@ -4,14 +4,27 @@
     <footer role="contentinfo" class="footer mt-auto py-3">
         <div class="container-fluid">
             <div class="container">
-                <div class="row">
-                    <div class="col-12 col-sm-6">
-                        <p class="pull-left my-4"><font-awesome-icon icon="code" class="mx-1"></font-awesome-icon> Maintained by <a href="https://github.com/lf-hernandez">Luis Felipe Hernandez</a>
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <p class="float-left">
+                            <font-awesome-icon icon="code" class="mx-1"></font-awesome-icon>
+                            <small>Created by <a class="authorAnchor" href="https://github.com/lf-hernandez">Luis Felipe Hernandez</a></small>
                         </p>
                     </div>
-                    <div class="col-12 col-sm-6">
-                        <img src="../../../assets/logo.svg" class="float-right ml-2" alt="Junkyard Strength Systems Logo" height="60px">
-                        <p class="float-right my-4">&copy; 2020 Junkyard Strength Systems. All rights reserved.</p>
+                    <div class="col-auto">
+                        <a class="brandIconAnchor mr-3" href="https://www.instagram.com/junkyard.strong/" target="_blank">
+                            <font-awesome-icon :icon="['fab', 'instagram']"></font-awesome-icon>
+                        </a>
+                        <a class="brandIconAnchor" href="mailto: junkyardstrengthsystems@gmail.com" target="_blank">
+                            <font-awesome-icon :icon="['fas', 'envelope']"></font-awesome-icon>
+                        </a>
+                    </div>
+                    <div class="col-auto">
+                        <p class="float-right">
+                            <small>
+                                &copy;{{new Date().getFullYear()}} Junkyard Strength Systems. All rights reserved.
+                            </small>
+                        </p>
                     </div>
                 </div>
             </div>
