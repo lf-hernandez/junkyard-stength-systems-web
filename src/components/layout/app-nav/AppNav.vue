08bb4f3461a49743cb96ae3c260a9290a15d7a54
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-23 00:10:07 -0400

    Fixed classes

--- src/components/layout/app-nav/AppNav.vue
+++ src/components/layout/app-nav/AppNav.vue
@@ -5,30 +5,19 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
             <div class="container">
-                <router-link class="appBrand" to="/">
-                    <img id="logo" src="../../../assets/logo.svg" alt="">
+                <router-link class="navbar-brand" to="/">
+                    <img class="jssLogoHeader" src="../../../assets/logo.svg" alt="Junkyard Strength Systems Logo">
                 </router-link>
-
-                <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                >
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navLinks" aria-controls="navLinks" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navLinks">
                     <div class="navbar-nav">
                         <router-link class="nav-item nav-link" :to="{name: 'clients'}">Clients</router-link>
                         <router-link class="nav-item nav-link" :to="{name: 'exercise-library'}">Exercise Library
                         </router-link>
                         <router-link class="nav-item nav-link" :to="{name: 'programs'}">Programs</router-link>
                     </div>
-
                     <div class="navbar-nav ml-md-auto">
                         <router-link class="nav-item nav-link" :to="{name: 'sign-in'}">Sign In</router-link>
                         <router-link class="nav-item nav-link" :to="{name: 'sign-up'}">Sign Up</router-link>
