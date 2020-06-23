b0142878862cc3d644d093155dd0dc651f1a00c8
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-23 00:10:23 -0400

    Registered brand icons

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/src/jquery.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+library.add(fab)
 library.add(fas);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
