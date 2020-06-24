8c2d7b0aa5300a0874b8c72ea479774186ab8dc2
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-06-24 01:28:59 -0400

    Added getClientFullName

--- src/store/modules/clients/getters.ts
+++ src/store/modules/clients/getters.ts
@@ -2,7 +2,10 @@ import { GetterTree } from 'vuex';
 import { Client, ClientsState, RootState } from '@/types/types';
 
 export const getters: GetterTree<ClientsState, RootState> = {
-    clientById: (state, id: string) => {
-        state.clients.find(client => client.id === id);
+    clientById(state, id: string) {
+        return state.clients.find(client => client.id === id);
+    },
+    clientFullName(state, id: string) {
+        return state.clients.find(client => client.id === id) ? (client: Client) => { `${ client.firstName } ${ client.lastName }` } : false;
     }
 };
