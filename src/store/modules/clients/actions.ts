e4bf1688422fdf081b89e09ef2ef6b75d315bf96
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-07-16 17:05:46 -0400

    add api calls

--- src/store/modules/clients/actions.ts
+++ src/store/modules/clients/actions.ts
@@ -4,25 +4,27 @@ import { ClientsState, RootState } from '@/types/types';
 
 import * as data from '../../../../data.json';
 
+// import api from '@/api';
+
 export const actions: ActionTree<ClientsState, RootState> = {
-    // TODO Service/API call
     async getClients({ commit }) {
         const clients = data.clients;
+        // const clients = await api.clients.getClients();
         commit('setClients', clients);
     },
     // TODO Service/API call
     async addClient({ commit }, client) {
-        // const addedClient = await clientService.addClient(client);
+        // const addedClient = await api.clients.addClient(client);
         commit('addClient', client);
     },
     // TODO Service/API
     async updateClient({ commit }, client) {
-        // const updatedClient = await clientService.updateClient(client);
+        // const updatedClient = await api.clients.updateClient(client);
         commit('updateClient', client);
     },
     // TODO Service/API
     async deleteClient({ commit }, client) {
-        // const deletedClient = await clientService.deleteClient(client);
+        // const deletedClient = await api.clients.deleteClient(client);
         commit('deleteClient', client);
     }
 };
