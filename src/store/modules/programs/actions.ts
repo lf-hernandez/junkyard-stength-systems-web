9d988624fbe2991057a2aec2a8d0801689554280
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-08-18 11:52:19 -0400

    Fix incorrect function names

--- src/store/modules/programs/actions.ts
+++ src/store/modules/programs/actions.ts
@@ -5,17 +5,17 @@ import { ProgramsState, RootState } from '@/types/types';
 import * as data from '../../../../data.json';
 
 export const actions: ActionTree<ProgramsState, RootState> = {
-    async getClients({ commit }) {
+    async getPrograms({ commit }) {
         const programs = data.programs;
         commit('setPrograms', programs);
     },
-    async addClient({ commit }, program) {
+    async addProgram({ commit }, program) {
         commit('addProgram', program);
     },
-    async updateClient({ commit }, program) {
+    async updateProgram({ commit }, program) {
         commit('updateProgram', program);
     },
-    async deleteClient({ commit }, program) {
+    async deleteProgram({ commit }, program) {
         commit('deleteProgram', program);
     }
 };
