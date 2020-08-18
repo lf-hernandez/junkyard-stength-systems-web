fd67299bf9fdd75ad4fc1f100896b362a125151f
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-08-18 12:14:48 -0400

    Register programs module

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,13 +5,15 @@ import { RootState } from '@/types/types';
 
 import { clients } from './modules/clients/index';
 import { exercises } from './modules/exercises/index';
+import { programs } from './modules/programs/index';
 
 Vue.use(Vuex);
 
 const options: StoreOptions<RootState> = {
     modules: {
         clients,
-        exercises
+        exercises,
+        programs
     }
 };
 
